a8f8d22fb60a0383244db1c536748c3cab39fbd3
xinhuatan <xinhuatan@tencent.com>
2022-07-31 11:19:35 +0800

    add forgoting to submit files

---
+++ src/wg_base/media_filter.cpp
@@ -0,0 +1,7 @@
+/**
+* Copyright 2022 xinhuatan  All rights reserved.
+* Author: tanxinhuaxt@163.com
+*/
+#include "media_filter.h"
+
+
---
+++ src/wg_base/media_filter.h
@@ -0,0 +1,17 @@
+/**
+* Copyright 2022 xinhuatan  All rights reserved.
+* Author: tanxinhuaxt@163.com
+*/
+#pragma once
+#include <QObject>
+#include <qwindow.h>
+class MediaFilter : public QObject {
+    Q_OBJECT
+ public:
+    explicit MediaFilter(QObject* parent = nullptr);
+
+ public slots:
+
+ signals:
+
+};
